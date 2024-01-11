4a76444c24ee19f88f87f016be64edffb9ab2e7d
Raluca <ralucaburduhos@gmail.com>
2024-01-11 18:03:30 +0200

    fullname utilizator

---
+++ Migrations/20240111155750_TabelaUtilizator.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Galerie_Arta_Web.Migrations
+{
+    public partial class TabelaUtilizator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Prenume",
+                table: "Utilizator",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nr_telefon",
+                table: "Utilizator",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Utilizator",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Prenume",
+                table: "Utilizator",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nr_telefon",
+                table: "Utilizator",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)",
+                oldMaxLength: 11,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Utilizator",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- Models/Utilizator.cs
+++ Models/Utilizator.cs
@@ -6,27 +6,26 @@ namespace Galerie_Arta_Web.Models
     {
         public int Id { get; set; }
 
-        [RegularExpression(@"^[A-Z][a-z]*\s?[A-Z]?[a-z]*$", ErrorMessage =
-            "Numele trebuie să înceapă cu majusculă (ex. Popescu sau Popescu Micu)")]
-        [StringLength(50, MinimumLength = 3)]
-        public string Name { get; set; }
+  
+        public string? Name { get; set; }
 
-        [RegularExpression(@"^[A-Z][a-z]*\s?[A-Z]?[a-z]*$", ErrorMessage =
-            "Prenumele trebuie să înceapă cu majusculă (ex. Ana sau Ana Maria)")]
-        [StringLength(50, MinimumLength = 3)]
-        public string Prenume { get; set; }
+
+        public string? Prenume { get; set; }
 
 
         [RegularExpression(@"^[0-9]{1,10}$", ErrorMessage = "Numărul de telefon trebuie să conțină doar cifre.")]
         [MaxLength(11, ErrorMessage = "Numărul de telefon trebuie să aibă maximum 10 cifre.")]
-        public string Nr_telefon { get; set; }
+        public string? Nr_telefon { get; set; }
 
         public string Email { get; set; }
-        public Utilizator()
+        [Display(Name = "Full Name")]
+        public string? FullName
         {
-            Email = "";
+            get
+            {
+                return Name + " " + Prenume;
+            }
         }
-
         public ICollection<Programare>? Programari { get; set; } //navigation property
     }
 }
